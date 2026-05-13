cddd777488d262f3dd2dc8d70e0bce52de1bc2f5
space-cee <bruhb6909@gmail.com>
2026-05-13 03:40:48 -0500

    Correct model name casing in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
-const model = "gemma-4-26B-A4B-it";
+const model = "gemma-4-26b-it";
 
 const app = express();
 const port = process.env.PORT || 3000;
